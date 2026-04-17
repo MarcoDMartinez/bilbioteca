199fdec638dc2caf1bb141a834de410b76d3420d
alan123 <20253rd030@utez.edu.mx>
2026-04-17 16:08:55 -0600

    arrgele el problema

--- src/main/java/com/example/mvcsesion/HelloApplication.java
+++ src/main/java/com/example/mvcsesion/HelloApplication.java
@@ -5,15 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("/com/example/view/view.fxml")
+        );
+
+        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
